c9a03c479ad6d1b64d42212aa5272dec117b82c4
ericjauregui <eric.jauregui@ymail.com>
2022-06-05 19:18:38 -0700

    small updates to print statement

--- send_sms.py
+++ send_sms.py
@@ -6,9 +6,11 @@ AUTH_TOKEN = credentials.get('auth_token')
 CLIENT = Client(ACCT_SID, AUTH_TOKEN)
 
 TGTS = credentials['targets']
-TEXT = 'MESSAGE'
-SENDER = credentials.get('twilio_us')
-SINGLE_TGT = -1
+TEXT = ''
+SENDER = credentials['twilio_us'][0]
+SINGLE_TGT = 0
+
+print(f'sending {TEXT} from {SENDER} to {TGTS[SINGLE_TGT]}')
 
 
 def send_letters(target, message, sender):
@@ -28,5 +30,5 @@ def send_sms(target, message, sender):
     return txt.sid
 
 
-for i in range(2):
-    print(send_sms(TGTS[SINGLE_TGT], TEXT, SENDER))
+# for _ in range(30):
+#     print(send_sms(TGTS[SINGLE_TGT], TEXT, SENDER))
