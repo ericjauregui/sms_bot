e95636f2a568a968eaa4ce77421f016d81c0ad5e
ericjauregui <eric.jauregui@ymail.com>
2022-07-04 18:06:50 -0700

    compartmentalized

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from twilio.rest import Client
+from secret_keys import credentials
 import send_sms
+
+ACCT_SID = credentials.get('account_sid')
+AUTH_TOKEN = credentials.get('auth_token')
+CLIENT = Client(ACCT_SID, AUTH_TOKEN)
+
+TEXT = ''
+SENDER = credentials['twilio_us'][0]
+
+TARGETS = []
+print(f'sending {TEXT} from {SENDER} to: {TARGETS}')
--- send_sms.py
+++ send_sms.py
@@ -1,34 +1,15 @@
-from twilio.rest import Client
-from secret_keys import credentials
-
-ACCT_SID = credentials.get('account_sid')
-AUTH_TOKEN = credentials.get('auth_token')
-CLIENT = Client(ACCT_SID, AUTH_TOKEN)
-
-TGTS = credentials['targets']
-TEXT = ''
-SENDER = credentials['twilio_us'][0]
-SINGLE_TGT = 0
-
-print(f'sending {TEXT} from {SENDER} to {TGTS[SINGLE_TGT]}')
-
-
-def send_letters(target, message, sender):
+def send_letters(client, target: str, message: str, sender: str) -> str:
     for i, m in enumerate(message):
         if m == ' ':
-            txt = CLIENT.messages.create(body=m + message[i + 1:],
+            txt = client.messages.create(body=m + message[i + 1:],
                                          from_=sender,
                                          to=target)
-            print(txt.sid)
+            return txt.sid
         else:
-            txt = CLIENT.messages.create(body=m, from_=sender, to=target)
-            print(txt.sid)
+            txt = client.messages.create(body=m, from_=sender, to=target)
+            return txt.sid
 
 
-def send_sms(target, message, sender):
-    txt = CLIENT.messages.create(body=message, from_=sender, to=target)
+def send_sms(client, target: str, message: str, sender: str) -> str:
+    txt = client.messages.create(body=message, from_=sender, to=target)
     return txt.sid
-
-
-# for _ in range(30):
-#     print(send_sms(TGTS[SINGLE_TGT], TEXT, SENDER))
